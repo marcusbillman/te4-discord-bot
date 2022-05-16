22a77676c464b2a58780ce171f6b8176ae110bc9
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-05-16 12:33:55 +0200

    ✨ Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { Client, Intents } = require('discord.js');
+const handleMessage = require('./message-handler');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  handleMessage(message);
+});
+
+client.login(process.env.TOKEN);
