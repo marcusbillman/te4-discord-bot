4c9cca70dbd08228848d30027184282cf6571b6c
ChristianBackstrom <christian.backstrom@elev.ga.ntig.se>
2022-05-17 11:03:20 +0200

    ✨ message-handler.js: Send segue and ad on message
    Co-authored-by: Marcus Billman <marcusbillman@users.noreply.github.com>

--- src/message-handler.js
+++ src/message-handler.js
@@ -1,7 +1,18 @@
-function handleMessage(message) {
-  if (message.content.startsWith('Hej')) {
-    message.reply('Hej hej :)');
-  }
+const database = require('./database');
+
+async function handleMessage(message) {
+  const segue = await database.getRandomSegue();
+  const ad = await database.getTopAdForQuery(message.content);
+  if (segue === null || ad === null) return;
+
+  const firstKeyword = ad.keywords.find((keyword) =>
+    message.content.includes(keyword)
+  );
+  const interpolatedSegue = segue.content
+    .replace('[N]', ad.name)
+    .replace('[K]', firstKeyword);
+
+  message.reply(`${interpolatedSegue}\n\n${ad.content}`);
 }
 
 module.exports = { handleMessage };
