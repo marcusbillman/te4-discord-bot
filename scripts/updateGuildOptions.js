fd045494e29ff5ddb9b1ef1d487b6f886c1938c0
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-05-19 13:52:15 +0200

    🔨 Add updateGuildOptions.js

---
+++ scripts/updateGuildOptions.js
@@ -0,0 +1,21 @@
+const database = require('../src/database');
+const { DEFAULT_GUILD_OPTIONS } = require('../src/constants');
+
+console.log('Updating guild options... Press Ctrl+C when output has stopped.');
+
+(async function () {
+  const guilds = await database.getAllGuilds();
+
+  guilds.forEach((guild) => {
+    const guildOptions = guild.options;
+
+    for (const [key, value] of Object.entries(DEFAULT_GUILD_OPTIONS)) {
+      if (guildOptions[key] == null) {
+        guildOptions[key] = value;
+        console.log(`Guild ${guild.guildId} - ${key}: ${value}`);
+      }
+    }
+
+    database.setGuildOptions(guild.guildId, guildOptions);
+  });
+})();
