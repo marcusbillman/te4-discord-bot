771bca4b983b0b4764c3e3d58f97fcf244580ac0
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-05-19 13:01:42 +0200

    ✨ ready.js: Log guild count

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,8 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(
+      `Logged in as ${client.user.tag} in ${client.guilds.cache.size} guilds`
+    );
   },
 };
