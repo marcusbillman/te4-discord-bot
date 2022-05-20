743121112d6d63265597d39104a8d5c49326a0e3
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-05-20 15:55:09 +0200

    ✨ Add '/stop' command

---
+++ src/commands/stop.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('stop')
+    .setDescription('Stops any audio ad playing in voice channels'),
+  async execute(interaction) {
+    const distube = interaction.client.distube;
+    const guildId = interaction.guild.id;
+
+    distube.stop(guildId);
+
+    await interaction.reply({
+      content: 'Stopped audio in voice channel',
+      ephemeral: true,
+    });
+  },
+};
